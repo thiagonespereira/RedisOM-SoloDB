9c9017a2a946e8fa15fae8ff345b7bd02c5d9eb2
Thiago Pereira <tlpereir@gmail.com>
2022-03-01 23:20:57 -0300

    Redis as single main database.

---
+++ pages/search/index.js
@@ -0,0 +1,10 @@
+import SearchForm from "../../lib/SearchForm";
+
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>Search a Car</h1>
+      <SearchForm />
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+};
